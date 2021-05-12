857127a5de517f35a0af247353f4ef1ca6db7a1f
oliverkardos <kardos.oliver@gmail.com>
2021-05-12 15:08:31 +0200

    getting database operations ready

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -140,6 +140,7 @@ namespace UrbanApp
             this.button2.TabIndex = 7;
             this.button2.Text = "Connect";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label5
             // 
--- Form1.cs
+++ Form1.cs
@@ -9,12 +9,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Security;
+using System.Data.SQLite;
+using System.Data.SqlClient;
 
 namespace UrbanApp
 {
     public partial class Form1 : Form
     {
         public string dbfilepath = null;
+        bool dbfileselected = false;
+        dbconnector sqliteconn = new dbconnector();
+        SQLiteCommand dbcommand;
+        bool connectionOpen = false;
 
         private Button selectButton;
         public Form1()
@@ -68,6 +74,33 @@ namespace UrbanApp
         private void btnWriteToDB_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dbfileselected)
+            {
+                if (!connectionOpen)
+                {
+                    sqliteconn.openConnection();
+                    connectionOpen = true;
+                    button2.BackColor = Color.Green;
+                }
+                dbcommand = new SQLiteCommand("SELECT * FROM urbanization", sqliteconn.GetConnection());
+                SQLiteDataReader dbreader = dbcommand.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(dbreader);
+
+                dt.Columns.Add("ID", typeof(int));
+                dt.Columns.Add("CoordX", typeof(string));
+                dt.Columns.Add("CoordY", typeof(string));
+                dt.Columns.Add("Name", typeof(string));
+                dt.Columns.Add("data1", typeof(int));
+                dt.Columns.Add("data2", typeof(int));
+
+            }
+            else MessageBox.Show("Error: no database file is selected, nothing to connect to");
         }
     }
 }
